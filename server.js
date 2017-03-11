f222ccd8af1f7a6e8c7cb1e988c1bca622c59f56
Administrator <admin@example.com>
2017-03-11 16:12:00 +0530

    db error connection hadle

--- server.js
+++ server.js
@@ -38,23 +38,7 @@ app.listen(process.env.PORT || 8100, function(req,res){
 
     _loadAPIs();
     console.log('apis loaded');
-    
-    global.connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });
-
-    global.connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-        }
-    });
-
+    dbconnection();
     console.log('database connected');
 
     console.log('server started listening at port 8100');
@@ -65,7 +49,7 @@ function _loadAPIs(){
     require('./api/user.js')();
     require('./api/booking.js')();
     require('./api/settings.js')();
-};
+}
 
 
 function _loadServices(){
@@ -73,4 +57,22 @@ function _loadServices(){
     global.userService = require('./service/user.js')();
     global.bookingService = require('./service/booking.js')();
     global.settingService = require('./service/settings.js')();
-};
+}
+
+function dbconnection() {
+    global.connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(dbconnection, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });
+
+    global.connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') { // Connection to the MySQL server is usually
+            dbconnection();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
